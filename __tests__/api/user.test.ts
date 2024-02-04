898924ef0a64c5e02c3110764c570207b16cb320
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-02-04 04:13:06 +0330

    testing api

--- __tests__/api/user.test.ts
+++ __tests__/api/user.test.ts
@@ -1,5 +1,39 @@
 import { testApiHandler } from 'next-test-api-route-handler'; // ◄ Must be first import
 import userHandler from '@/pages/api/users/index';
+import userReservationsHandler from '@/pages/api/users/[userId]/reservations';
+jest.mock('@/lib/auth/utils')
+test('GET api/users/[userId]/reservations receives check token', async () => {
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (param) => {
+      param.userId = 1;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({
+        method: 'GET',
+      });
+      expect(res.status).toBe(200);
+      const json = await res.json();
+      expect(json).toHaveProperty('userReservations');
+      expect(json.userReservations).toHaveLength(2);
+    },
+  });
+});
+jest.mock('@/lib/auth/utils');
+test('GET /user/[userId]/reservations  not receives', async () => {
+  await testApiHandler({
+    handler: userReservationsHandler,
+    paramsPatcher: (param) => {
+      param.userId = 123456;
+    },
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: 'GET' });
+      expect(res.status).toBe(200);
+      const dataRes = await res.json();
+      expect(dataRes.userReservations).toHaveLength(0);
+    },
+  });
+});
 
 test('POST api/users receives token with correct credentials', async () => {
   await testApiHandler({
@@ -19,7 +53,15 @@ test('POST api/users receives token with correct credentials', async () => {
       expect(res.status).toBe(200);
 
       const json = await res.json();
-      console.log(json);
+      
+      expect(json).toHaveProperty('user')
+      expect(json.user).toHaveProperty('id')
+      expect(json.user.id).toEqual(1)
+      expect(json.user).toHaveProperty('email')
+      expect(json.user.email).toEqual('test@test.test');
+      expect(json.user).toHaveProperty('token');
+
+
     },
   });
 });
---
+++ lib/auth/__mocks__/utils.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  esModule:true,
+  validateToken : jest.fn().mockResolvedValue(true)
+};
+
+export { }
+// mock module in test file using this code
+// jest.mock('@/lib/auth/utils')
